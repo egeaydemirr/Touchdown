83c41e4cfd7c37e76232cb713e3ba454c9002197
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-21 16:55:31 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Ege Aydemir on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
