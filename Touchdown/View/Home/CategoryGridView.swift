22d0b3750f76b15d57084f942ebcd942ad2644e0
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-22 19:58:49 +0300

    CategoryGridView

---
+++ Touchdown/View/Home/CategoryGridView.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryGridView.swift
+//  Touchdown
+//
+//  Created by Ege Aydemir on 22.10.2022.
+//
+
+import SwiftUI
+
+struct CategoryGridView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
+                ForEach(categories) { category in
+                    CategoryItemView(category: category)
+                }
+            })//:GRID
+            .frame(height: 140)
+            .padding(.horizontal, 15)
+            .padding(.vertical,10)
+            
+        })//:SCROLL
+     
+    }
+}
+// MARK: - PREVIEW
+struct CategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryGridView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
