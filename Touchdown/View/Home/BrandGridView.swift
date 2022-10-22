66b518fd3db05082c66e17e4ccae0d4590203c7f
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-22 21:36:05 +0300

    BrandGridView

---
+++ Touchdown/View/Home/BrandGridView.swift
@@ -0,0 +1,34 @@
+//
+//  BrandGridView.swift
+//  Touchdown
+//
+//  Created by Ege Aydemir on 22.10.2022.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, spacing: columnSpacing, content: {
+                ForEach(brands) { brand in
+                    BrandItemView(brand: brand)
+                }
+            })//:GRID
+            .frame(height: 200)
+            .padding(15)
+        })//:SCROLL
+    }
+}
+
+// MARK: - PREVIEW
+struct BrandGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandGridView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
