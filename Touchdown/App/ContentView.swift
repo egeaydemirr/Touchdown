a7ad5e8d47dab9468a09cc5e3616434a5ae9064b
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-22 15:46:09 +0300

    Implement the ContentView

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -12,8 +12,22 @@ struct ContentView: View {
     
     // MARK: - BODY
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing:0) {
+                NavigationBarView()
+                    .padding()
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }//:VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }//:ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
+        
     }
 }
 // MARK: - PREVIEW
