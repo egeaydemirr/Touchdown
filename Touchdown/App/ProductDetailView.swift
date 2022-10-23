475d7985bd71d7c3f9ecb3b794ce8399b545ed39
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-23 15:08:46 +0300

    CreateProductDetailView

---
+++ Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,36 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by Ege Aydemir on 23.10.2022.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5){
+            //NAVBAR
+            //HEADER
+            Text(sampleProduct.name)
+            //DETAIL TOP PART
+            //DETAIL BOTTO MBAR
+            //RATINGS + SIZES
+            //DESCRIPTION
+            //QUANTITY + FAVOURITE
+            //ADD TO CARD
+            Spacer()
+        }//:VSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+            .previewLayout(.fixed(width: 375, height: 812))
+    }
+}
