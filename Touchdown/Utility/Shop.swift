184d8bac55b62392ead97b9bf685f60d647d3777
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-25 15:19:24 +0300

    Shop

---
+++ Touchdown/Utility/Shop.swift
@@ -0,0 +1,13 @@
+//
+//  Shop.swift
+//  Touchdown
+//
+//  Created by Ege Aydemir on 25.10.2022.
+//
+
+import Foundation
+
+class Shop {
+    var shoppingProduct: Bool = false
+    var selectedProduct: Product? = nil
+}
